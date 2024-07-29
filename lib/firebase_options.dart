b5d38a2b9f6b8410c5a459b0831f3375a60dda05
jonathancaleb <jonathancaleb230@gmail.com>
2024-07-29 11:46:08 +0300

    :secret: "modification secrets"

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -46,29 +47,29 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAj3jMDfKrhoB4KYEgkdx56MwFA_WvpSCY',
-    appId: '1:433132540338:web:017bf426a956fcbfda54ab',
-    messagingSenderId: '433132540338',
-    projectId: 'eapp-56f9a',
-    authDomain: 'eapp-56f9a.firebaseapp.com',
-    storageBucket: 'eapp-56f9a.appspot.com',
+  static final FirebaseOptions web = FirebaseOptions(
+    apiKey: dotenv.env['WEB_API_KEY']!,
+    appId: dotenv.env['WEB_APP_ID']!,
+    messagingSenderId: dotenv.env['WEB_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['WEB_PROJECT_ID']!,
+    authDomain: dotenv.env['WEB_AUTH_DOMAIN']!,
+    storageBucket: dotenv.env['WEB_STORAGE_BUCKET']!,
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBYfB5S-Wfh448kF_0PQvkorh9vxB_KbFA',
-    appId: '1:433132540338:android:3c17ac3254062a2ada54ab',
-    messagingSenderId: '433132540338',
-    projectId: 'eapp-56f9a',
-    storageBucket: 'eapp-56f9a.appspot.com',
+  static final FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY']!,
+    appId: dotenv.env['ANDROID_APP_ID']!,
+    messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['ANDROID_PROJECT_ID']!,
+    storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET']!,
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAf3FHpRPXdnfrZlmcivc3Ng0gNigx-3s0',
-    appId: '1:433132540338:ios:e5fe08f7d526ca65da54ab',
-    messagingSenderId: '433132540338',
-    projectId: 'eapp-56f9a',
-    storageBucket: 'eapp-56f9a.appspot.com',
-    iosBundleId: 'com.example.eapp',
+  static final FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY']!,
+    appId: dotenv.env['IOS_APP_ID']!,
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['IOS_PROJECT_ID']!,
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET']!,
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID']!,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  await dotenv.load();
+  runApp(MyApp());
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
